5f802f3a5312de1fc810fd8fc14641cc3b71220b
Leandro <le.s.kuranaga@hotmail.com>
2021-01-10 13:34:14 -0300

    Add Footer

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './styles.css';
-import { ReactComponent as Logo } from './logo.svg';
 
-const Footer = () => {
+
+const Orders = () => {
     return (
         <nav className="main-navbar">
-            <Logo />
             <a href="/" className="logo-text">DS Delivery</a>
         </nav>
     )
 }
 
-export default Footer;
+export default Orders;

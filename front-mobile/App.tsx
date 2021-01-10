94a8594a60283cc73bbffa585af7b0b2022e19ca
Leandro <le.s.kuranaga@hotmail.com>
2021-01-10 20:13:56 -0300

    Add Home Page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Header from "./src/Header/index";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_700Bold,
+} from "@expo-google-fonts/open-sans";
+import Home from "./src/Home";
 
-export default function App() {
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
